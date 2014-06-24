25ac31a2e5c18b6b60e7d03930fe8186ce6b54f7
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2014-06-24 21:44:10 +0700

    Clean this dirty source using "make mrproper" command

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Mar 14 15:53:48 KST 2014"
-#define LINUX_COMPILE_BY "omsgsm"
-#define LINUX_COMPILE_HOST "sy918-desktop"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
